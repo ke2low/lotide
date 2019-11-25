05413e67974d01061817ed380e7b85af81146d13
Kalvin Low <ke2low@uwaterloo.ca>
2019-11-25 19:36:10 +0000

    assertEqual.js initial commit

---
+++ assertEqual.js
@@ -0,0 +1,33 @@
+// FUNCTION IMPLEMENTATION
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.assert(actual === expected,'"Lighthouse Labs" !== "Bootcamp"');
+//   }
+//   else {
+//     console.assert(actual === expected, " === ");
+//   }
+// };
+
+//throwing a completely different erorr message compared to what is written in the exercise, might be because it is a different version of node
+
+// const assertEqual = function(actual, expected) {
+//   try {
+//     console.assert(actual === expected);
+//     console.log("✅✅✅ Assertion Passed: " + actual + " !== " + expected);
+//   } catch (e) {
+//     console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+//   }
+// };
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} !== ${expected}`);
+  }
+  else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
