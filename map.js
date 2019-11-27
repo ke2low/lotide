215f4cb13fd3eae441ed22d5e03efd37805bd0d7
Kalvin Low <ke2low@uwaterloo.ca>
2019-11-27 21:30:36 +0000

    complete

---
+++ map.js
@@ -0,0 +1,43 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length != arr2.length) {
+    return false;
+  }
+  else if (arr1.length == 0 && arr2.length == 0)  {
+    return true;
+  }
+  else {
+    for (let i = 0; i < arr1.length; i++) {
+      // console.log (arr1[i] + " " + arr2[i])
+      if (arr1[i] != arr2[i]) {
+        return false;
+      }
+      else if ((i == arr1.length - 1 && (arr1[i] === arr2[i]))) {
+        return true;
+      }
+    }
+  }
+}
+
+const assertArraysEqual = function(arr1, arr2)  {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} == ${arr2}`);
+  }
+  else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback)  {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+assertArraysEqual(results1, ["g","c","t","m","t"]);
