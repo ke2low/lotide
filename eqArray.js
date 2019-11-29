25b0a3c826d89f8001780128ef87a6bc405c5562
Kalvin Low <ke2low@uwaterloo.ca>
2019-11-29 17:41:25 +0000

    modified eqArray to work on nested arrays

--- eqArray.js
+++ eqArray.js
@@ -7,23 +7,49 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// const eqArrays = function(arr1, arr2) {
+//   if (arr1.length != arr2.length) {
+//     console.log("Test")
+//     return false;
+//   }
+//   else {
+//     for (let i = 0; i < arr1.length; i++) {
+//       console.log (arr1[i] + " " + arr2[i])
+//       if (arr1[i] != arr2[i]) {
+//         console.log("Test")
+//         return false;
+//       }
+//       else if (i == arr1.length - 1 && (arr1[i] === arr2[i])) {
+//         return true;
+//       }
+//     }
+//   }
+// }
+
 const eqArrays = function(arr1, arr2) {
+  let matching = true;
   if (arr1.length != arr2.length) {
-    console.log("Test")
-    return false;
+    matching = false;
   }
   else {
     for (let i = 0; i < arr1.length; i++) {
-      console.log (arr1[i] + " " + arr2[i])
-      if (arr1[i] != arr2[i]) {
-        console.log("Test")
-        return false;
-      }
-      else if (i == arr1.length - 1 && (arr1[i] === arr2[i])) {
-        return true;
+      if (Array.isArray(arr1[i]) == true) {
+        matching = eqArrays(arr1[i], arr2[i]);
+      } else {
+        if (arr1[i] !== arr2[i])  {
+          matching = false;
+        }
+        else if (arr1[i] == arr2[i]) {
+          matching = true;
+        }
       }
     }
   }
+  return matching;
 }
 
+
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true) // => true
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false)// => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false) // => false
