ba0d6374ef48ad853a2659b89ef58ec1b6d8ed08
Kalvin Low <ke2low@uwaterloo.ca>
2019-12-02 14:35:11 +0000

    refactored test code into a seperate directory made for tests

--- flatten.js
+++ flatten.js
@@ -1,5 +1,3 @@
-// let newArr = [];
-
 const assertArraysEqual = function(arr1, arr2)  {
   if (eqArrays(arr1, arr2)) {
     console.log(`✅✅✅ Assertion Passed: ${arr1} == ${arr2}`);
--- head.js
+++ head.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} !== ${expected}`);
-  }
-  else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 function head(arr) {
   if (arr.length == 0) {
@@ -16,8 +9,4 @@ function head(arr) {
   }
 }
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([]), undefined);
-assertEqual(head([5]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
